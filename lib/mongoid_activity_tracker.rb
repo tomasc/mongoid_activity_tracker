893029e118fc98936af222f4d4ad5c9f7129bffe
Tomas Celizna <tomas.celizna@gmail.com>
2014-08-15 14:08:05 +0200

    fix requires

--- lib/mongoid_activity_tracker.rb
+++ lib/mongoid_activity_tracker.rb
@@ -1,1 +1,4 @@
-require "mongoid_activity_tracker/version"
+require 'mongoid_activity_tracker/version'
+
+require 'mongoid_activity_tracker/track_activity'
+require 'mongoid_activity_tracker/tracker'
